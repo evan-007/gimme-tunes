e4d313558ed389ac722224eb321812f8fc82ce1f
Evan <eulloyd@gmail.com>
2014-04-11 14:13:31 +0900

    Add ajax results to DOM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+var addSentences = function(object) {
+	console.log(object+' im inside the addSentences function') //this works
+	for (var count = 0; count < object.length; count++) {
+		$('body').find('.container').append('<p>'+object[count]+'</p>');
+	}
+	// var results = $('.template').clone();
+	// for (var n = 0; n > object.length; n++) {
+
+	// 	console.log(object[n]);
+	// 	$('body').append('<p>'+object[n]+'</p>');
+	// };
+}
+
+
+
 $(document).ready(function(){
 	$('#urlGetter').on('submit', function(){
 		var url = $(this).find('input[name="url"]').val();
@@ -5,6 +20,8 @@ $(document).ready(function(){
 		getSummary(url);
 	});
 })
+
+
 
 
 var getSummary = function(inputUrl) {
@@ -15,9 +32,14 @@ var getSummary = function(inputUrl) {
 		data: { url: inputUrl },
 
 		success: function(response){
-			console.log(response);
+			console.log(response); //debug
+			console.log(response.sentences[0]+' this is sentence 1');
+			addSentences(response.sentences);
 		},
 
 		type: "GET"
 	}
+
 )};
+
+
