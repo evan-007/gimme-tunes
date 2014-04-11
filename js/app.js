da874d64444b8ae6133ba272d8227966bfd05ca2
Evan <eulloyd@gmail.com>
2014-04-11 11:12:08 +0900

    Add textApi call

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('#urlGetter').on('submit', function(){
+		var url = $(this).find('input[name="url"]').val();
+		console.log(url); //just for debug
+		getSummary(url);
+	});
+})
+
+
+var getSummary = function(inputUrl) {
+	$.ajax('https://aylien-text.p.mashape.com/summarize', {
+
+		beforeSend: function (request) {
+            request.setRequestHeader("X-Mashape-Authorization", 'M2sq25zthb1e6XHg6JNmJgS8rZEn7IAM');
+        },
+        
+		data: { url: inputUrl },
+
+		success: function(response){
+			console.log(response);
+		},
+
+		type: "GET"
+		}
+	)}
