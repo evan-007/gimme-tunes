c5e63ece170cc93573cf224412ae9bc3428162cd
Evan <eulloyd@gmail.com>
2014-04-11 16:08:31 +0900

    Add other function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var addSentences = function(object) {
-	console.log(object+' im inside the addSentences function') //this works
+	// console.log(object+' im inside the addSentences function') //this works
 	for (var count = 0; count < object.length; count++) {
 		$('body').find('.container').append('<p>'+object[count]+'</p>');
 	}
@@ -17,14 +17,14 @@ $(document).ready(function(){
 	$('#urlGetter').on('submit', function(){
 		var url = $(this).find('input[name="url"]').val();
 		console.log(url); //just for debug
-		getSummary(url);
+		getSentences(url);
+		// getArticle(url);
 	});
 })
 
 
 
-
-var getSummary = function(inputUrl) {
+var getSentences = function(inputUrl) {
 	$.ajax('https://aylien-text.p.mashape.com/summarize', {
 
 		headers: { "X-Mashape-Authorization": 'M2sq25zthb1e6XHg6JNmJgS8rZEn7IAM' },
@@ -32,8 +32,8 @@ var getSummary = function(inputUrl) {
 		data: { url: inputUrl },
 
 		success: function(response){
-			console.log(response); //debug
-			console.log(response.sentences[0]+' this is sentence 1');
+			// console.log(response); //debug
+			// console.log(response.sentences[0]+' this is sentence 1');
 			addSentences(response.sentences);
 		},
 
@@ -42,4 +42,20 @@ var getSummary = function(inputUrl) {
 
 )};
 
+var getArticle = function(inputUrl) {
+	$.ajax('https://aylien-text.p.mashape.com/extract'), {
 
+		headers: { "X-Mashape-Authorization": 'M2sq25zthb1e6XHg6JNmJgS8rZEn7IAM' },
+
+		data: { url: inputUrl },
+
+		success: function(response){
+			console.log(response); //debug
+			console.log('this is the title: '+response.title)
+			console.log('this is the article: '+response.article);
+		},
+
+		type: "GET"
+
+	}
+}
