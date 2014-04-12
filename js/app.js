9ae8116176e7d92dba017ca76ecdceecf7b31f21
Evan Lloyd <eulloyd@gmail.com>
2014-04-12 17:39:23 +0900

    clone youtube format

--- js/app.js
+++ js/app.js
@@ -91,7 +91,8 @@ var getVideos = function(query) {
 var addVideos = function(videos) {
 	console.log(videos.items);
 	$.each(videos.items, function(index, video) {
-		console.log(video.id.videoId);
+		console.log(video.id.videoId); //just testing
+		var newVid = $('.hidden.col-md-4').clone()
 		$('#videos').append('<iframe width="560" height="315" src="//www.youtube.com/embed/'+
 			video.id.videoId+'" frameborder="0" allowfullscreen></iframe>')
 	})
